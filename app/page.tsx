d64dcc96d7f0fbaf1e1c834b7559d48c2b45247d
me-jameshwartlopez <me@jameshwartlopez.com>
2024-03-30 19:15:50 +0800

    test: neon db

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import prisma from '@/app/db';
-import { TodoList } from '@/app/components/TodoList';
-import { CreateTodo } from '@/app/components/forms/CreateTodo';
+import prisma from '@/app/db'
+import { TodoList } from '@/app/components/TodoList'
+import { CreateTodo } from '@/app/components/forms/CreateTodo'
 
 async function getData() {
   const data = await prisma.todo.findMany({
@@ -11,24 +11,22 @@ async function getData() {
     orderBy: {
       createdAt: 'desc',
     },
-  });
+  })
 
-  return data;
+  return data
 }
 
 export default async function Home() {
-  const data = await getData();
+  const data = await getData()
   return (
-    <div className="h-screen w-screen flex flex-col gap-8 items-center justify-center">
-      <h1 className="text-center text-4xl font-bold">
-        CRUD APP - WITH SUPABASE
-      </h1>
-      <div className="border rounded-lg shadow-xl p-10 w-1/3">
+    <div className='h-screen w-screen flex flex-col gap-8 items-center justify-center'>
+      <h1 className='text-center text-4xl font-bold'>CRUD APP - WITH NEON</h1>
+      <div className='border rounded-lg shadow-xl p-10 w-1/3'>
         <CreateTodo />
-        <div className="mt-5 flex flex-col gap-y-2">
+        <div className='mt-5 flex flex-col gap-y-2'>
           <TodoList data={data} />
         </div>
       </div>
     </div>
-  );
+  )
 }
